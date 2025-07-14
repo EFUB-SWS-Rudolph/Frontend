d79921aacbef947f744405a88a1884c7793e4146
corjqnrl <coqnrl@ewha.ac.kr>
2025-07-14 17:24:58 +0900

    🔥Delete: delete test code

--- sws/src/App.jsx
+++ sws/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Main from './main/page/Main';
-import SignIn from './signIn/pages/SignIn';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Route element={<Layout />}>
           {/* <Route path="/" element={<Signup />} /> */}
           <Route path="/" element={<Main />} />
-          <Route path="/signin" element={<SignIn />} />
         </Route>
       </Routes>
     </BrowserRouter>
