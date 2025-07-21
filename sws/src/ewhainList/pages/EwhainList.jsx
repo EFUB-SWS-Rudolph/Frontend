45e1c4d2ac7060a42efe787b95f3d7546dad6cd9
corjqnrl <coqnrl@ewha.ac.kr>
2025-07-21 20:09:44 +0900

    💄Design: add no result component

--- sws/src/ewhainList/pages/EwhainList.jsx
+++ sws/src/ewhainList/pages/EwhainList.jsx
@@ -5,8 +5,11 @@ import EwhainListHeader from '../components/common/EwhainListHeader';
 import HeaderFilter from '../components/common/HeaderFilter';
 import UserCard from '../components/common/UserCard';
 import USERS from '../constants/users';
+import { FaFaceMeh } from "react-icons/fa6";
 
 export default function EwhainList() {
+  const searchExist = true;  // api 연결 후 검색 결과 여부 표시
+
   return (
     <EwhainListWrapper>
       <HeaderContainer>
@@ -14,14 +17,24 @@ export default function EwhainList() {
         <HeaderFilter/>
       </HeaderContainer>
       
-      <EwhainContainer>
-        {USERS.map((user) => (
-          <UserCard
-            user={user}
-            key={user.id}
-          />
-        ))}
-      </EwhainContainer>
+      { searchExist ? 
+      <>
+        <EwhainContainer>
+          {USERS.map((user) => (
+            <UserCard
+              user={user}
+              key={user.id}
+            />
+          ))}
+        </EwhainContainer>
+      </> :
+      <>
+        <NoResult>
+          <FaFaceMeh size={50} styled={{ color: theme.colors.gray500 }} />
+          검색 결과가 없어요 :{'('}
+        </NoResult>
+      </>
+      }
       
       <NavigationBar>내비게이션바위치</NavigationBar>
     </EwhainListWrapper>
@@ -63,6 +76,25 @@ const EwhainContainer = styled.div`
   &::-webkit-scrollbar {
     display: none;
   }
+`;
+
+const NoResult = styled.div`
+  width: 356px;
+  margin-top: 10px;
+  margin-right: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  flex: 1;
+  color: ${({ theme }) => theme.colors.gray500};
+  font-family: ${({ theme }) => theme.fonts.display.body.large.fontFamily};
+  font-size: ${({ theme }) => theme.fonts.display.body.large.fontSize};
+  font-style: ${({ theme }) => theme.fonts.display.body.large.fontStyle};
+  font-weight: ${({ theme }) => theme.fonts.display.body.large.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.display.body.large.lineHeight};
 `;
 
 const NavigationBar = styled.div`
