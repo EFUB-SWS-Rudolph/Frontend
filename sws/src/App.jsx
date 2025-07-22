d9900c139a7c1f1f7d96ef3d7833321d6747239e
corjqnrl <coqnrl@ewha.ac.kr>
2025-07-22 12:42:48 +0900

    🧪Test: add test route

--- sws/src/App.jsx
+++ sws/src/App.jsx
@@ -1,6 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Main from './main/page/Main';
+import EwhainList from './ewhainList/pages/EwhainList';
+import EwhainFilter from './ewhainList/pages/EwhainFilter';
+import ExchangeFilter from './ewhainList/pages/ExchangeFilter';
+import MajorFilter from './ewhainList/pages/MajorFilter';
+import PeriodFilter from './ewhainList/pages/PeriodFilter';
+import DeptFilter from './ewhainList/pages/DeptFilter';
 
 function App() {
   return (
@@ -9,6 +15,12 @@ function App() {
         <Route element={<Layout />}>
           {/* <Route path="/" element={<Signup />} /> */}
           <Route path="/" element={<Main />} />
+          <Route path="/ewhainlist" element={<EwhainList />} />
+          <Route path="/ewhainfilter" element={<EwhainFilter />} />
+          <Route path="/ewhainfilter/exchange" element={<ExchangeFilter />} />
+          <Route path="/ewhainfilter/dept" element={<DeptFilter />} />
+          <Route path="/ewhainfilter/dept/major" element={<MajorFilter />} />
+          <Route path="/ewhainfilter/period" element={<PeriodFilter />} />
         </Route>
       </Routes>
     </BrowserRouter>
