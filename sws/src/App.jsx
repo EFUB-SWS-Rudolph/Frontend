60a0c31d822d99dc17b30e37e3b90feb02cf324f
corjqnrl <coqnrl@ewha.ac.kr>
2025-07-22 15:59:54 +0900

    🔥Delete: delete test code

--- sws/src/App.jsx
+++ sws/src/App.jsx
@@ -1,11 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Main from './main/page/Main';
-import Certification from './signUp/pages/Certification';
-import UnivInfo from './signUp/pages/UnivInfo';
-import SetProfile from './signUp/pages/SetProfile';
-import SetInterest from './signUp/pages/SetInterest';
-import SetTalent from './signUp/pages/SetTalent';
 
 function App() {
   return (
@@ -14,11 +9,6 @@ function App() {
         <Route element={<Layout />}>
           {/* <Route path="/" element={<Signup />} /> */}
           <Route path="/" element={<Main />} />
-          <Route path="/signup/certification" element={<Certification />} />
-          <Route path="/signup/univ" element={<UnivInfo />} />
-          <Route path="/signup/profile" element={<SetProfile />} />
-          <Route path="/signup/interest" element={<SetInterest />} />
-          <Route path="/signup/talent" element={<SetTalent />} />
         </Route>
       </Routes>
     </BrowserRouter>
