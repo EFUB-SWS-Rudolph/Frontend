a215569795a97a89ed4ed3448319bdd000e16dba
corjqnrl <coqnrl@ewha.ac.kr>
2025-07-23 23:08:24 +0900

    💄Design: TalentInterestItem component

---
+++ sws/src/ewhainList/components/TalentInterestItem.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import theme from '../../styles/theme';
+
+export default function TalentInterestItem({ item }) {
+  return (
+    <ItemContainer>
+      {item}
+    </ItemContainer>
+  );
+}
+
+const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 101px;
+  height: 36px;
+  background: ${({ theme }) => `
+    linear-gradient(${theme.colors.white}, ${theme.colors.white}) padding-box,
+    linear-gradient(to right, ${theme.colors.primary}, ${theme.colors.forth}) border-box
+  `};
+
+  border: 1px solid transparent;
+  border-radius: 12px;
+  font-family: ${({ theme }) => theme.fonts.body.medium.fontFamily};
+  font-size: ${({ theme }) => theme.fonts.body.medium.fontSize};
+  font-style: ${({ theme }) => theme.fonts.body.medium.fontStyle};
+  font-weight: ${({ theme }) => theme.fonts.body.medium.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.body.medium.lineHeight};
+  color: ${({ theme }) => theme.colors.black};
+`;
