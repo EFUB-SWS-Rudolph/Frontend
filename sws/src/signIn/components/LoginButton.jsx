7f74104330644b32e1b5016909e4098b52e841c8
corjqnrl <coqnrl@ewha.ac.kr>
2025-07-25 21:25:47 +0900

    💄Design: edit login page ui

--- sws/src/signIn/components/LoginButton.jsx
+++ sws/src/signIn/components/LoginButton.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { FcGoogle } from 'react-icons/fc';
 import KAKAO from '../icons/icon_kakao.svg?react';
 import GOOGLE from '../icons/logo_google.svg?react';
 import theme from '../../styles/theme';
@@ -7,50 +6,53 @@ import theme from '../../styles/theme';
 export default function LoginButton( {method, color} ) {
   return (
     <LoginBtnContainer color={color} theme={theme}>
-        <IconWrapper>
-          {(method==="카카오") ? <KAKAO width="1.25rem" height="1.25rem" flex-shrink="0" /> : 
-          <FcGoogle width="2.4375rem" height="2.4375rem" flex-shrink="0" aspect-ratio="1/1" />}
-        </IconWrapper>
-          <MethodText>{method} 로그인</MethodText>
+      {(method==="카카오") ? 
+        <IconContainer>
+          <KAKAO width="2rem" height="2rem" flex-shrink="0" />
+        </IconContainer>
+      : 
+        <IconContainer>
+          <GOOGLE style={{width:"2.4375rem", height:"2.4375rem", flexShrink:"0", aspectRatio:"1/1", borderRadius:"3.125rem", background:"url(<path-to-image>) lightgray 50% / cover no-repeat"}} />
+        </IconContainer>
+      }
+      <MethodText color={color}>{method} 로그인</MethodText>
     </LoginBtnContainer>
   );
 }
 
 const LoginBtnContainer = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: row;
+  width: 21.3125rem;
+  height: 3rem;
+  display: inline-flex;
+  padding: ${({ color }) => 
+    color === 'yellow' ? "0.5rem 7.9375rem 0.5rem 1rem" : "0.5rem 8.375rem 0.5rem 0.9375rem"
+  };
   align-items: center;
-  justify-content: center;
-  width: 341px;
-  height: 48px;
-  gap: 10px;
-  border-radius: 30px;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-  padding: 14px 20px;
-  margin: 8px 20px;
-  cursor: pointer;
-  font-family: ${({ theme }) => theme.fonts.display.title.medium.fontFamily};
-  font-size: ${({ theme }) => theme.fonts.display.title.medium.fontSize};
-  font-style: ${({ theme }) => theme.fonts.display.title.medium.fontStyle};
-  font-weight: ${({ theme }) => theme.fonts.display.title.medium.fontWeight};
-  line-height: ${({ theme }) => theme.fonts.display.title.medium.lineHeight};
-  color: ${({ theme }) => theme.colors.black};
-  background-color: ${({ color, theme }) =>
-    color === 'yellow' ? '#fee500' : theme.colors.white};
+  gap: ${({ color }) => 
+    color === 'yellow' ? "4.9375rem" : "5.4375rem"
+  };
+  border-radius: 1.5rem;
+  background: ${({ color }) => 
+    color === 'yellow' ? "var(--Kakao, #FEE500)" : "var(--White, #FFF)"
+  };
 `;
 
-const IconWrapper = styled.div`
+const IconContainer = styled.div`
   display: flex;
-  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  padding: ${({ color }) => 
+    color === 'yellow' ? "0.375rem" : "0"
+  };
   justify-content: center;
-  position: absolute;
-  left: 20px;
-  top: 50%;
-  transform: translateY(-50%);
+  align-items: center;
 `;
 
 const MethodText = styled.div`
+  width: ${({ color }) => 
+    color === 'yellow' ? "5.4375rem" : "4.5625rem"
+  };
+  height: 1.1875rem;
   flex: 1 0 0;
   color: ${({ theme }) => theme.colors.black};
   text-align: center;
--- sws/src/signIn/pages/SignIn.jsx
+++ sws/src/signIn/pages/SignIn.jsx
@@ -6,9 +6,8 @@ import theme from '../../styles/theme';
 export default function SignIn() {
   return (
     <Wrapper>
-      <Spacer />
       <LoginPhraseContainer>
-        <WeevoLogo width="132px" height="28px" />
+        <WeevoLogo width="8.25rem" height="1.75rem" />
         <LoginPhrase>로그인</LoginPhrase>
       </LoginPhraseContainer>
       <LoginBtnContainer>
@@ -23,29 +22,32 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
-  background-color: #f7f6f3;
-  justify-content: space-between;
-`;
-
-const Spacer = styled.div`
-  flex: 0.1;
+  background-color: ${({ theme }) => theme.colors.background};
+  padding: 0;
+  margin: 0;
 `;
 
 const LoginPhraseContainer = styled.div`
   display: flex;
+  width: 8.25rem;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  gap: 1rem;
+  margin: 18.5rem 8.06rem 16rem;
+  padding: 0;
 `;
 
 const LoginPhrase = styled.h3`
+  align-self: stretch;
   color: ${({ theme }) => theme.colors.primary};
+  text-align: center;
   font-family: ${({ theme }) => theme.fonts.display.title.large.fontFamily};
   font-size: ${({ theme }) => theme.fonts.display.title.large.fontSize};
   font-style: ${({ theme }) => theme.fonts.display.title.large.fontStyle};
   font-weight: ${({ theme }) => theme.fonts.display.title.large.fontWeight};
   line-height: ${({ theme }) => theme.fonts.display.title.large.lineHeight};
-  margin: 10px 0;
+  margin: 0;
+  padding: 0;
 `;
 
 const LoginBtnContainer = styled.div`
@@ -54,5 +56,7 @@ const LoginBtnContainer = styled.div`
   justify-content: center;
   align-items: center;
   padding-bottom: 50px;
-  gap: 10px;
+  gap: 1rem;
+  margin: 0 1.5rem 1.81rem;
+  padding: 0;
 `;
