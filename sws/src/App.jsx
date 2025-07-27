cd3e172cfd78686354c05c3e75ffe2c32995ab2f
corjqnrl <coqnrl@ewha.ac.kr>
2025-07-27 20:54:20 +0900

    🧪Test: add test route

--- sws/src/App.jsx
+++ sws/src/App.jsx
@@ -2,6 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Main from './main/page/Main';
 import MyPage from './mypage/page/MyPage';
+import EwhainList from './ewhainList/pages/EwhainList';
+import EwhainFilter from './ewhainList/pages/EwhainFilter';
+import ExchangeFilter from './ewhainList/pages/ExchangeFilter';
+import MajorFilter from './ewhainList/pages/MajorFilter';
+import PeriodFilter from './ewhainList/pages/PeriodFilter';
+import DeptFilter from './ewhainList/pages/DeptFilter';
 
 function App() {
   return (
@@ -11,6 +17,12 @@ function App() {
           {/* <Route path="/" element={<Signup />} /> */}
           <Route path="/" element={<Main />} />
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/ewhainlist" element={<EwhainList />} />
+          <Route path="/ewhainfilter" element={<EwhainFilter />} />
+          <Route path="/ewhainfilter/exchange" element={<ExchangeFilter />} />
+          <Route path="/ewhainfilter/dept" element={<DeptFilter />} />
+          <Route path="/ewhainfilter/dept/major" element={<MajorFilter />} />
+          <Route path="/ewhainfilter/period" element={<PeriodFilter />} />
         </Route>
       </Routes>
     </BrowserRouter>
