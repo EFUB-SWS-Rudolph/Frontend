4f612f2aac76abb84feeeeb087464a2308478b0f
corjqnrl <coqnrl@ewha.ac.kr>
2025-07-29 01:40:55 +0900

    💄Design: add tag edit version

--- sws/src/mypage/components/myTags/Tag.jsx
+++ sws/src/mypage/components/myTags/Tag.jsx
@@ -1,10 +1,16 @@
 //나의재능 관심분야 tag
 
 import styled from 'styled-components';
+import DELETE from '../../assets/icon_delete.svg?react';
+import { useProfileStore } from '../../stores/ProfileStore';
+
 export default function Tag({ text }) {
+  const { talentTags, interestTags, isEditing, setTalentTags, setInterestTags, removeTalentTag, removeInterestTag } = useProfileStore();
+
   return (
     <Container>
       <Text>{text}</Text>
+      {isEditing && <Delete />}
     </Container>
   );
 }
@@ -35,4 +41,10 @@ const Text = styled.div`
   font-style: normal;
   font-weight: 500;
   line-height: 150%;
+`;
+
+const Delete = styled(DELETE)`
+  width: 0.53125rem;
+  height: 0.49331rem;
+  flex-shrink: 0;
 `;
