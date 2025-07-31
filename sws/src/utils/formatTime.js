4346aebfe17da1fd0e50e3d22d700fb2320a36a1
seoyonara <syp614@ewhain.net>
2025-07-31 22:56:44 +0900

    ✨ Feat: add formatTime util

---
+++ sws/src/utils/formatTime.js
@@ -0,0 +1,17 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
+
+export const formatTime = (dateString) => {
+  const target = dayjs(dateString);
+  const now = dayjs();
+
+  if (now.diff(target, 'day') >= 30) {
+    return target.format('YYYY.MM.DD');
+  }
+
+  return target.fromNow();
+};
