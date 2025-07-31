8662bea8800974ebf814e6bae1beb21ce1c51525
corjqnrl <coqnrl@ewha.ac.kr>
2025-07-31 21:12:17 +0900

    🎨Feat: 토글 버튼 상태 추가

--- sws/src/mypage/stores/ProfileStore.js
+++ sws/src/mypage/stores/ProfileStore.js
@@ -2,45 +2,57 @@ import { create } from 'zustand';
 
 export const useProfileStore = create((set) => ({
   nickname: '',
-  profileImg: '',
-  college: '엘텍공과대학',
-  department: '서양화과',
-  studentid: '24',
-  location: '서울시 강동구',
-  talentTags: [{ id: Date.now(), tag: '바이올린' }],
-  interestTags: [{ id: Date.now(), tag: '프로그래밍' }, { id: Date.now(), tag: '작곡' }],
-  isEditing: false,
-  isOnChoice: false,
-
   setNickname: (name) => set({ nickname: name }),
+
+  profileImg: '',
   setProfileImg: (img) => set({ profileImg: img }),
+
+  college: '엘텍공과대학',
   setCollege: (tag) => set({ college: tag }),
+
+  department: '서양화과',
   setDepartment: (tag) => set({ department: tag }),
+
+  studentid: '24',
   setStudentid: (tag) => set({ studentid: tag }),
+
+  location: '서울시 강동구',
   setLocation: (tag) => set({ location: tag }),
 
+  talentTags: [{ id: Date.now(), tag: '바이올린' }],
   setTalentTags: (tags) => set({ talentTags: tags }),
-  setInterestTags: (tags) => set({ interestTags: tags }),
-
   addTalentTag: (tag) => 
     set((state) => ({
       talentTags: [...state.talentTags, { id: Date.now() + Math.random(), tag }],
   })),
+  removeTalentTag: (id) => set((state) => ({
+    talentTags: state.talentTags.filter((i) => i.id !== id),
+  })),
+  resetInterestTags: () => set({ interestTags: [] }),
+
+  interestTags: [{ id: Date.now(), tag: '프로그래밍' }, { id: Date.now(), tag: '작곡' }],
+  setInterestTags: (tags) => set({ interestTags: tags }),
   addInterestTag: (tag) => 
     set((state) => ({
       interestTags: [...state.interestTags, { id: Date.now() + Math.random(), tag }],
   })),
-
-  setIsEditing: (flag) => set({ isEditing: flag }),
-  setIsOnChoice: (flag) => set({ isOnChoice: flag }),
-
-  removeTalentTag: (id) => set((state) => ({
-    talentTags: state.talentTags.filter((i) => i.id !== id),
-  })),
   removeInterestTag: (id) => set((state) => ({
     interestTags: state.interestTags.filter((i) => i.id !== id),
   })),
-
-  resetInterestTags: () => set({ interestTags: [] }),
   resetTalentTags: () => set({ talentTags: [] }),
+
+  isExchange: true,
+  setIsExchange: (flag) => set({ isExchange: flag }),
+
+  isDonation: true,
+  setIsDonation: (flag) => set({ isDonation: flag }),
+
+  isCoffeeChat: true,
+  setIsCoffeeChat: (flag) => set({ isCoffeeChat: flag }),
+
+  isEditing: false,
+  setIsEditing: (flag) => set({ isEditing: flag }),
+
+  isOnChoice: false,
+  setIsOnChoice: (flag) => set({ isOnChoice: flag }),
 }));
