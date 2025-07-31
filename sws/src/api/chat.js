bf3c5b8b17790e4e79ec22b4c0617e7a7edb88c3
seoyonara <syp614@ewhain.net>
2025-07-31 23:03:18 +0900

    ✨ Feat: add chat api function

---
+++ sws/src/api/chat.js
@@ -0,0 +1,59 @@
+import { client } from './client';
+
+export const getChatroomExists = async (opponentId, courseId) => {
+  try {
+    const res = await client.get(`/chat/rooms/exist?opponentId=${opponentId}&courseId=${courseId}`);
+    console.log(res.data);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const postChatroom = async (data) => {
+  try {
+    const res = await client.post(`/chat/rooms`, data);
+    console.log(res.data);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getChatroomList = async (category) => {
+  try {
+    const res = await client.get(`/chat/rooms?category=${category}`);
+    console.log(res.data);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getMessageList = async (chatroomId, pageNum, size) => {
+  try {
+    const res = await client.get(`/chat/rooms/${chatroomId}/messages?page=${pageNum}&size=${size}`);
+    console.log(res.data);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const patchMessageRead = async (chatroomId) => {
+  try {
+    const res = await client.patch(`/chat/rooms/${chatroomId}/messages/read`);
+    console.log(res.data);
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const deleteChatroom = async (chatroomId) => {
+  try {
+    const res = await client.delete(`/chat/rooms/${chatroomId}`);
+    console.log(res.data);
+  } catch (err) {
+    throw err;
+  }
+};
