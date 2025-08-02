b2448b9135ed293a87577c0eb842e51dd0ec10c7
seoyonara <syp614@ewhain.net>
2025-08-03 03:21:42 +0900

    ♻️ Refactor: App.js 컴포넌트 분리

---
+++ sws/src/main/components/LectureListHeaderContent.jsx
@@ -0,0 +1,116 @@
+import { Link, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import AddIconURL from '../../common/assets/icons/btn_add.svg';
+
+export const LectureListHeaderContent = () => {
+  const location = useLocation();
+  return (
+    <LecturePageHeaderContainer>
+      <LecturePageTitleArea>
+        <div style={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+          <LectureTitle>강의</LectureTitle>
+        </div>
+        <AddButton>
+          <img src={AddIconURL} alt="추가" style={{ width: '24px', height: '24px' }} />
+        </AddButton>
+      </LecturePageTitleArea>
+      {/* 탭 바 (MainLectureTabsContainer) */}
+      <MainLectureTabsContainer>
+        <MainLectureTab
+          to="/lectures/search"
+          $active={location.pathname === '/lectures' || location.pathname === '/lectures/search'}
+        >
+          강의 조회
+        </MainLectureTab>
+        <MainLectureTab
+          to="/lectures/recommend"
+          $active={location.pathname === '/lectures/recommend'}
+        >
+          강의 추천
+        </MainLectureTab>
+        <MainLectureTab to="/lectures/my" $active={location.pathname === '/lectures/my'}>
+          내 강의
+        </MainLectureTab>
+      </MainLectureTabsContainer>
+    </LecturePageHeaderContainer>
+  );
+};
+
+// 강의 목록 페이지 헤더 관련 요소들
+const LectureTitle = styled.h2`
+  font-family: 'Pretendard Variable', sans-serif;
+  font-weight: 600;
+  font-size: 1.25rem;
+  line-height: 100%;
+  letter-spacing: 0rem;
+  color: #222222;
+  margin: 0;
+  position: absolute;
+  top: 0.94rem;
+  left: 11.125rem;
+`;
+const AddButton = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0.94rem;
+  left: 21.85rem;
+`;
+// [강의네비게이션바] (MainLectureTabsContainer)
+const MainLectureTabsContainer = styled.div`
+  width: 22.313rem;
+  height: 3.125rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  gap: 0.063rem;
+  background-color: #ffffff;
+  position: absolute;
+  top: 3.5rem;
+  left: 1rem;
+`;
+const MainLectureTab = styled(Link)`
+  flex: 1;
+  padding: 0.625rem 0;
+  text-decoration: none;
+  background-color: ${(props) => (props.$active ? '#FFFFFF' : 'transparent')};
+  color: ${(props) => (props.$active ? '#00664F' : '#999999')};
+  border: none;
+  border-bottom: ${(props) => (props.$active ? '4px solid #00664F' : 'none')};
+  font-size: 1rem;
+  font-weight: ${(props) => (props.$active ? 'bold' : 'normal')};
+  cursor: pointer;
+  outline: none;
+  transition: all 0.2s ease-in-out;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LecturePageHeaderContainer = styled.div`
+  width: 100%;
+  height: 6.62rem;
+  background: #ffffff;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 0 1.5rem;
+  position: relative;
+`;
+// 강의 페이지 헤더의 타이틀 및 추가 버튼 섹션
+const LecturePageTitleArea = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.5rem;
+  height: 6.375rem;
+  box-sizing: border-box;
+`;
